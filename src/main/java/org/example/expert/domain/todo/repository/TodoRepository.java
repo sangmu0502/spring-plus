245043325e2edbae3266dd7eb7728d6e09961377
sangmu <dltkdandl@naver.com>
2025-12-29 15:50:46 +0900

    lv2-3 : QueryDSL을 사용한 단건 일정 조회

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -8,9 +8,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoCustomRepository {
 
     @Query("""
           SELECT t FROM Todo t LEFT JOIN FETCH t.user u
@@ -23,9 +22,4 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
                                               @Param("weather") String weather,
                                               @Param("startDateTime") LocalDateTime startDateTime,
                                               @Param("endDateTime") LocalDateTime endDateTime);
-
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 }
